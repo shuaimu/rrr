725c20de1ae4e8d76cad33b6c519c9881cef84ed
lmx <liumengxing2010@qq.com>
2015-03-19 11:03:49 +0800

    save for merge

---
+++ test/s.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+ > File Name: s.cpp
+ > Author: Mengxing Liu
+ > Created Time: 2015-03-19
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	cout << "server starts.." << endl;
+
+}
